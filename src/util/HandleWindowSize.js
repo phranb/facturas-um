b2472f09dea4696e209ca056467a7c91250a23f3
Franco Battagion <fran.battagion@gmail.com>
2021-10-13 14:23:59 -0300

    HandleWindowSize detecta dispositivo movil

---
+++ src/util/HandleWindowSize.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+export const HandleWindowSize = () => {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  function handleWindowSizeChange() {
+    setWidth(window.innerWidth);
+  }
+  useEffect(() => {
+    window.addEventListener("resize", handleWindowSizeChange);
+    return () => {
+      window.removeEventListener("resize", handleWindowSizeChange);
+    };
+  }, []);
+  let isMobile = Boolean(width <= 768);
+  return isMobile;
+};
